1c8259458a791e675d7b3e32cbbdf379784bc67f
lucvo01 <109111777+lucvo01@users.noreply.github.com>
2023-05-11 22:11:54 +0000

    gi

--- src/features/todo/actions.js
+++ src/features/todo/actions.js
@@ -1,14 +1,20 @@
 import { ADD_TODO, TOGGLE_TODO, SET_FILTER } from "./reducer";
+import apiService from "../../app/apiService";
+
 let nextTodoId = 0;
 
-export const addTodo = (text) => {
-  return {
-    type: ADD_TODO,
-    payload: {
+export const addTodo = (text) => async (dispatch) => {
+  try {
+    const todo = {
       id: nextTodoId++,
-      text
-    }
-  };
+      text,
+      completed: false
+    };
+    const res = await apiService.post("/todos", todo);
+    dispatch({type: ADD_TODO, payload: {id: todo.id, text}})
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const toggleTodo = (id) => {
